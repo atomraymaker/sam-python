89b1ed51ca1ce8c6ebfe0d113946f0686973aa43
Adam Baker <abaker@trek10.com>
2020-07-09 20:46:51 -0400

    python version, deps, scripts

--- {{ cookiecutter.project_name }}/README.md
+++ {{ cookiecutter.project_name }}/README.md
@@ -3,11 +3,3 @@
 ## Install Dependencies
 
 `PIPENV_VENV_IN_PROJECT=true pipenv sync --dev`
-
-## Run Tests
-
-`pipenv run pytest`
-
-## Run Shell
-
-`pipenv run python -i shell.py`
--- {{ cookiecutter.project_name }}/app/handler.py
+++ {{ cookiecutter.project_name }}/app/handler.py
@@ -1,11 +1,10 @@
 import boto3
+
 import util
-from logger import LOG
 
 SESSION = boto3.session.Session()
 
 
 def handle(event, _context):
-    LOG.info("Loaded event", lambda_input=event)
     word = event.get("word", "")
     return util.upcase_string(word, SESSION)
